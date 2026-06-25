1896503a5576c455f9c5deb6071a0c41737412d3
RafhyKhan <Rafhy.mk@hotmail.com>
2026-06-25 15:15:34 -0600

    AppDriver file changes

--- src/sait/mms/application/AppDriver.java
+++ src/sait/mms/application/AppDriver.java
@@ -2,6 +2,8 @@ package sait.mms.application;
 
 public class AppDriver {
 
+	
+	//Starts the program
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/sait/mms/problemdomain/Vehicle.java
+++ src/sait/mms/problemdomain/Vehicle.java
@@ -2,9 +2,32 @@ package sait.mms.problemdomain;
 
 public class Vehicle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*
+	
+	//Constructor
+	public Vehicle() {
+		
 	}
+	
+	//Check if available vehicle
+	public boolean isAvaliable() {
+		return true;
+	}
+	
+	
+	//Checkout vehicle, purchasing, eliminating for txt file
+	public void checkout() {
+		
+	}
+	
+	
+	//return vehicle info via string
+	public String toString() {
+		
+		return "A String";
+	}
+	
+	*/
+	
 
 }
